ffaaf06521e983a68b522f975fb7b1d1cb77ee1f
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-04-06 22:10:15 +0530

    context

--- pr-3/src/App.js
+++ pr-3/src/App.js
@@ -1,21 +1,11 @@
-import Card from "../src/components/card"
-import React, { useState } from 'react';
+import React from 'react'
+import UserContextProvider from './context/UserContextProvider'
 
-function App() {
-  let [count,setCount] = useState(0)
-  let myobj = {
-   name:"himanshi",
-   age:19
-  }
-
+const App = () => {
   return (
-    <div className="flex items-center justify-center flex-col p-10">
-      <h1>Hello</h1>
-      <Card user="Himanshi" tex="Prince" />
-      <br />
-      <Card user="Prince" />
-    </div>
-  );
+    <UserContextProvider>
+    </UserContextProvider>
+  )
 }
 
-export default App;
+export default App
